94f259795f401bb3abfcf1abfc2920abe08749c9
lyminhnghia <lmn2461999@gmail.com>
2020-04-06 12:55:32 +0700

    add connection authen in socket

--- controllers/game/connection.js
+++ controllers/game/connection.js
@@ -1,9 +1,17 @@
 const db = require('../../configs/db.config')
-const User = db.user
-module.exports = (io, token) => {
+const sequelize = db.sequelize
+const socketJwt = require('socketio-jwt')
 
-    io.on('connection', socket => {
-        console.log('connection')
+module.exports = (io) => {
+
+    io.on('connection', socketJwt.authorize({
+        secret: 'uet-team-secret',
+        timeout: 10000
+    })).on('authenticated', async socket => {
+        const user = await sequelize.query(`Select username from users WHERE id = ${socket.decoded_token.id}`,{
+            type: sequelize.QueryTypes.SELECT
+        })
+        console.log(user[0].username)
     })
 
 }
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 module.exports = (app, io) => {
     const authJwt       =       require('./verifyJwtToken')
     const user          =       require('../controllers/user/user')
-    const connection    =       require('../controllers/game/connection')(io, authJwt.verifyToken)
+    const connection    =       require('../controllers/game/connection')(io)
 
     app.post('/api/login', [authJwt.checkUsername, authJwt.checkPassword], user.login)
     app.post('/api/register', [authJwt.checkUsername, authJwt.checkPassword], user.register)
