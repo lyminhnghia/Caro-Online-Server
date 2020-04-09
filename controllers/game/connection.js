35cc25a37c9154ea54bcaeb836340485529071f0
lyminhnghia <lmn2461999@gmail.com>
2020-04-10 00:10:22 +0700

    push

--- controllers/game/connection.js
+++ controllers/game/connection.js
@@ -5,87 +5,119 @@ const socketJwt = require('socketio-jwt')
 // const waiting = require('./room/waiting-room')
 
 module.exports = (io) => {
-    const freePlayers = {}
+    const players = {}
     const map = []
-    const busyPlayers = {}
-    const freeRooms = {}
-    const busyRooms = {}
+    const rooms = {}
     io.on('connection', socketJwt.authorize({
         secret: 'uet-team-secret',
         timeout: 10000
     })).on('authenticated', async socket => {
-        const user = await sequelize.query(`Select username, elo, isLocalImage, imageUrl from users WHERE id = ${socket.decoded_token.id}`,{
+        const user = (await sequelize.query(`Select username, elo, isLocalImage, imageUrl from users WHERE id = ${socket.decoded_token.id}`,{
             type: sequelize.QueryTypes.SELECT
-        })
+        }))[0]
 
-        if (map[user[0].username]) {
-            console.log(map)
+        if (map[user.username]) {
             io.sockets.connected[socket.id].disconnect()
         } else {
-            map[user[0].username] = socket.id
+            map[user.username] = socket.id
         }
 
         socket.emit('server ready')
 
-        freePlayers[socket.id] = {
-            username            : user[0].username,
-            elo                 : user[0].elo,
-            isLocalImage        : user[0].isLocalImage,
-            imageUrl            : user[0].imageUrl
+        players[socket.id] = {
+            username            : user.username,
+            elo                 : user.elo,
+            isLocalImage        : user.isLocalImage,
+            imageUrl            : user.imageUrl,
+            currentRoom         : null
         }
         
-        socket.on('information', data => {
-            console.log(data)
-            socket.emit('information', freePlayers[socket.id])
+        socket.on('information', () => {
+            socket.emit('information', {
+                username : user.username,
+                elo: user.elo,
+                isLocalImage : user.isLocalImage,
+                imageUrl : user.imageUrl
+            })
         })
 
         socket.on('list player', () => {
-            socket.emit('list player', freePlayers)
+            let result = []
+            for (i in players) {
+                let player = players[i]
+                if (player.currentRoom) {
+                    continue
+                }
+                result.push({
+                    username : player.username,
+                    isLocalImage : player.isLocalImage,
+                    imageUrl : player.imageUrl,
+                    elo : player.elo
+                })
+            }
+            socket.emit('list player', result)
         })
 
         socket.on('list room', () => {
-            let rooms = []
-            for (i in freeRooms) {
-                let host = busyPlayers[map[freeRooms[i].hostname]]
-                let havePassword = freeRooms[i].password !== ''
-                rooms.push({
+            let result = []
+            for (i in rooms) {
+                let room = rooms[i]
+                if (room.joinname) {
+                    continue
+                }
+                let host = room.hostname
+                let havePassword = room.password !== ''
+                result.push({
                     username: host.username,
                     isLocalImage: host.isLocalImage,
                     imageUrl: host.imageUrl,
-                    timelapse: freeRooms[i].timelapse,
-                    rank: freeRooms[i].rank,
+                    timelapse: room.timelapse,
+                    rank: room.rank,
                     elo: host.elo,
                     havePassword : havePassword
                 })
             }
-            socket.emit('list room', rooms)
+            socket.emit('list room', result)
         })
 
         socket.on('create room', data => {
-            freeRooms[socket.id] = {
-                hostname        : freePlayers[socket.id].username,
+            rooms[user.username] = {
+                hostname        : user.username,
                 joinname        : null,
                 password        : data.password,
                 timelapse       : data.timelapse,
                 rank            : data.rank
             }
         
-            socket.join(socket.id)
+            socket.join(user.username)
 
             havePassword = data.password !== ''
 
             io.emit('create room', {
-                username            : freePlayers[socket.id].username,
-                elo                 : freePlayers[socket.id].elo,
-                isLocalImage        : freePlayers[socket.id].isLocalImage,
-                imageUrl            : freePlayers[socket.id].imageUrl,
+                username            : user.username,
+                elo                 : user.elo,
+                isLocalImage        : user.isLocalImage,
+                imageUrl            : user.imageUrl,
                 rank                : data.rank,
                 timelapse           : data.timelapse,
                 havePassword        : havePassword
             })
 
-            busyPlayers[socket.id] = freePlayers[socket.id]
-            delete freePlayers[socket.id]
+            players[socket.id].currentRoom = user.username
+        })
+
+        socket.on('join room', data => {
+            if (rooms[data.username].password === data.password) {
+                rooms[data.username].joinname = user.username
+                players[socket.id].currentRoom = data.username
+                socket.join(data.username)
+                socket.to(data.username).emit('join room', {
+                    username : user.username,
+                    isLocalImage : user.isLocalImage,
+                    imageUrl : user.imageUrl,
+                    elo : user.elo
+                })
+            }
         })
 
         // socket.on('delete room', () =>{
@@ -103,18 +135,19 @@ module.exports = (io) => {
         socket.on('disconnect', () => {
 
             console.log(`Disconnected: ${socket.id}`)
-            if (map[user[0].username] !== socket.id) {
+            if (map[user.username] !== socket.id) {
                 return
             }
-            delete map[user[0].username]
-            if (freeRooms[socket.id]) {
-                delete freeRooms[socket.id]
+            let player = players[socket.id]
+            if (player.currentRoom) {
+                socket.to(player.currentRoom).emit("quit")
+                if (player.currentRoom == player.username) {
+                    players[map[rooms[player.currentRoom].joinname]].currentRoom = null  
+                    delete rooms[player.currentRoom]
+                }
             }
-            if (freePlayers[socket.id]) {
-                delete freePlayers[socket.id]
-            } else {
-                delete busyPlayers[socket.id]
-            }
+            delete map[user.username]
+            delete players[socket.id]
         })
     })
 
