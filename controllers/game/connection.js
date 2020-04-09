e2e8adad4507917027b88fa3ca4ab261b18e506f
lyminhnghia <lmn2461999@gmail.com>
2020-04-09 17:08:53 +0700

    update

--- controllers/game/connection.js
+++ controllers/game/connection.js
@@ -5,8 +5,10 @@ const socketJwt = require('socketio-jwt')
 // const waiting = require('./room/waiting-room')
 
 module.exports = (io) => {
-    const players = {}
-    const rooms = {}
+    const freePlayers = {}
+    const busyPlayers = {}
+    const freeRooms = {}
+    const busyRooms = {}
     io.on('connection', socketJwt.authorize({
         secret: 'uet-team-secret',
         timeout: 10000
@@ -17,54 +19,66 @@ module.exports = (io) => {
 
         socket.emit('server ready')
 
-        players[socket.id] = {
+        freePlayers[socket.id] = {
             username            : user[0].username,
             elo                 : user[0].elo,
             isLocalImage        : user[0].isLocalImage,
-            imageUrl            : user[0].imageUrl,
-            roomListen          : false,
-            playerListen        : true
+            imageUrl            : user[0].imageUrl
         }
         
         socket.on('call information', data => {
             console.log(data)
-            io.emit('information player', players[socket.id])
+            io.emit('information player', freePlayers[socket.id])
         })
 
-        socket.on('create room', data => {
-            rooms[players[socket.id].username] = {
-                userId          : socket.id,
-                password        : data.password,
-                timelapse       : data.timelapse,
-                rank            : data.rank
-            }
-            socket.join(data.username)
-
-            havePassword = data.password === ''
-
-            io.emit('create room', {
-                username            : players[socket.id].username,
-                elo                 : players[socket.id].elo,
-                isLocalImage        : players[socket.id].isLocalImage,
-                imageUrl            : players[socket.id].imageUrl,
-                rank                : data.rank,
-                timelapse           : data.timelapse,
-                havePassword        : havePassword
-            })
+        socket.on('list player', () => {
+            io.emit('list player', freePlayers)
         })
 
-        socket.on('listen room', listen => {
-            players[socket.id].roomListen = listen
-            if (listen) {
-                io.emit('listen room', rooms)
-            }
-        })
+        // socket.on('create room', data => {
+        //     rooms[freePlayers[socket.id].username] = {
+        //         hostId          : socket.id,
+        //         joinId          : null,
+        //         password        : data.password,
+        //         timelapse       : data.timelapse,
+        //         rank            : data.rank
+        //     }
+        //     socket.join(data.username)
+
+        //     havePassword = data.password === ''
+
+        //     io.emit('create room', {
+        //         username            : players[socket.id].username,
+        //         elo                 : players[socket.id].elo,
+        //         isLocalImage        : players[socket.id].isLocalImage,
+        //         imageUrl            : players[socket.id].imageUrl,
+        //         rank                : data.rank,
+        //         timelapse           : data.timelapse,
+        //         havePassword        : havePassword
+        //     })
+        // })
+
+        // socket.on('delete room', () =>{
+        //     delete rooms[players[socket.id].username]
+        //     io.emit('delete room', players[soc])
+        // })
+
+        // socket.on('listen room', listen => {
+        //     players[socket.id].roomListen = listen
+        //     if (listen) {
+        //         io.emit('listen room', rooms)
+        //     }
+        // })
 
         socket.on('disconnect', () => {
 
             console.log(`Disconnected: ${socket.id}`)
 
-            delete players[socket.id]
+            if (freePlayers[socket.id]) {
+                delete freePlayers[socket.id]
+            } else {
+                delete busyPlayers[socket.id]
+            }
 
             io.emit('disconnect', socket.id)
         })
