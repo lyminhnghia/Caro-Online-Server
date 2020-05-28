4ea116d61501a8ce2f948c0cccb4c8923b178bc2
lyminhnghia <lmn2461999@gmail.com>
2020-05-28 17:24:05 +0700

    update version

--- controllers/game/leaderboard.js
+++ controllers/game/leaderboard.js
@@ -1,3 +1,6 @@
+const db = require('../../configs/db.config')
+const sequelize = db.sequelize
+
 const LeaderBoard = (socket) => {
     socket.on('leaderboard', async () => {
         topRanking = (await sequelize.query(`Select username, elo, imageUrl from users ORDER BY elo DESC LIMIT 10`,{
