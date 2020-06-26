00d68d6e7bfe412a63ec30fad5dbb4f963e21b2a
thanhvh99 <43335196+thanhvh99@users.noreply.github.com>
2020-06-26 23:13:55 +0700

    Update checkboard.js

--- controllers/game/checkboard.js
+++ controllers/game/checkboard.js
@@ -1,23 +1,35 @@
 const CheckBoard = (board, x, y) => {
-    for (let offsetX = -1; offsetX <= 1; offsetX++) {
-        for (let offsetY = -1; offsetY <= 1; offsetY++) {
+    for (let offsetX = 0; offsetX <= 1; offsetX++) {
+        for (let offsetY = 0; offsetY <= 1; offsetY++) {
             if (offsetX === 0 && offsetY === 0) {
                 continue
             }
-            flag = true
+            count = 1
             for (let i = 1; i < 5; i++) {
                 let tempX = x + offsetX * i
                 let tempY = y + offsetY * i
                 if (tempX < 0 || tempX >= 19 || tempY < 0 || tempY >= 19) {
-                    flag = false
                     break
                 }
                 if (board[tempY][tempX] != board[y][x]) {
-                    flag = false
                     break
+                } else {
+                    count++
                 }
             }
-            if (flag) {
+            for (let i = 1; i < 5; i++) {
+                let tempX = x - offsetX * i
+                let tempY = y - offsetY * i
+                if (tempX < 0 || tempX >= 19 || tempY < 0 || tempY >= 19) {
+                    break
+                }
+                if (board[tempY][tempX] != board[y][x]) {
+                    break
+                } else {
+                    count++
+                }
+            }
+            if (count >= 5) {
                 return true
             }
         }
