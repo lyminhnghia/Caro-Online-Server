aa94a53f41bc6f3fe080ae35ad5dab43fd8e854c
lyminhnghia <lmn2461999@gmail.com>
2020-06-28 21:00:35 +0700

    update

--- controllers/game/statistic.js
+++ controllers/game/statistic.js
@@ -13,6 +13,10 @@ const Statistic = (socket) => {
             type: sequelize.QueryTypes.SELECT
         })
 
+        result.rank = (await sequelize.query(`SELECT (1 + (SELECT count(*) FROM users a WHERE a.elo > b.elo )) AS ranking FROM users b WHERE username = '${socket.user.username}'`,{
+            type: sequelize.QueryTypes.SELECT
+        })).ranking
+
         socket.emit('statistic', result)
 
     })
